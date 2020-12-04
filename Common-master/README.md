1b39a71ca869568e049c9a5b6cef76c1de5813ad
dshilkret <dshilkret@gmail.com>
2020-12-04 10:04:22 -0600

    deletions

--- Common-master/README.md
+++
@@ -1,9 +0,0 @@
-# Raydreams.Common
-
-Combines several projects under one repo including Raydreams.Common, Raydreams.Common.Services and the Raydreams.Web web site.
-
-Added in the FormEncrypt Examples for now.
-
-## Tech Stack
-
-.NET Core 3.1 MVC Razor with jQuery
--- Common-master/Raydreams.Web/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-function isEmpty(s)
-{
-    return s === null || s === undefined ? true : /^[\s\xa0]*$/.test(s);
-}
